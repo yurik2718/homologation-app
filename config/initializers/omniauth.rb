fdb7437ac664c0d36ab4ba99e429da1373581afb
yurik2718 <andrei.iurik@gmail.com>
2026-04-18 07:32:36 +0300

    fix CI

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,12 +3,12 @@ OmniAuth.config.silence_get_warning = true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
-    Rails.application.credentials.dig(:google, :client_id) || "placeholder",
-    Rails.application.credentials.dig(:google, :client_secret) || "placeholder",
+    ENV["GOOGLE_CLIENT_ID"] || Rails.application.credentials.dig(:google, :client_id) || "placeholder",
+    ENV["GOOGLE_CLIENT_SECRET"] || Rails.application.credentials.dig(:google, :client_secret) || "placeholder",
     { scope: "email,profile", prompt: "select_account" }
 
   provider :facebook,
-    Rails.application.credentials.dig(:facebook, :app_id) || "placeholder",
-    Rails.application.credentials.dig(:facebook, :app_secret) || "placeholder",
+    ENV["FACEBOOK_APP_ID"] || Rails.application.credentials.dig(:facebook, :app_id) || "placeholder",
+    ENV["FACEBOOK_APP_SECRET"] || Rails.application.credentials.dig(:facebook, :app_secret) || "placeholder",
     { scope: "email,public_profile", info_fields: "email,name" }
 end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,1 @@
-Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+Stripe.api_key = ENV["STRIPE_SECRET_KEY"] || Rails.application.credentials.dig(:stripe, :secret_key)
